74f353c97fc23b07bcf3514705035f0e6d745724
Ericson Scodeler <ericson.br@hotmail.com>
2020-07-15 11:02:48 -0300

    criando formulario

--- pages/api/save.js
+++ pages/api/save.js
@@ -30,21 +30,21 @@ export default async(req,res) =>{
       Promo = textCell.value
     }
 
-
-
-
-    // Nome	Email	Whatsapp	Cupom	Promo
     await sheet.addRow({
       Nome: data.Nome,
       Email: data.Email,
       Whatsapp: data.Whatsapp,
-      Nota: 5,
+      Nota: parseInt(data.Nota),
       'Data Preenchimento': moment().format('DD/MM/YYYY HH:mm:ss'),
       Cupom,
       Promo
     })
 
-    res.end(req.body)
+    res.end(JSON.stringify({
+      showCoupon: Cupom !== '',
+      Cupom,
+      Promo
+    }))
   }catch(err){
     console.log(err)
     res.end('error')
